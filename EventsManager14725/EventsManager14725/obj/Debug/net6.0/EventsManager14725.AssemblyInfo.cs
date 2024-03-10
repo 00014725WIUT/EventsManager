7473ca9b68067f662da13f614197ef1bdc0113a4
00014725WIUT <162869156+00014725WIUT@users.noreply.github.com>
2024-03-11 00:00:27 +0500

    Added EventsManager MVC

--- EventsManager14725/EventsManager14725/obj/Debug/net6.0/EventsManager14725.AssemblyInfo.cs
+++ EventsManager14725/EventsManager14725/obj/Debug/net6.0/EventsManager14725.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventsManager14725")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7640d9dc224b8795537c8ea6c57f2ad2cf07f28")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventsManager14725")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventsManager14725")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
